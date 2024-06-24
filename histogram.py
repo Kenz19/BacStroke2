44675f72e92946fbba71be5bc9597ec6c099c716
Kenzie <110031493+Kenz19@users.noreply.github.com>
2024-06-24 15:52:40 +0100

    created general plotting function for histogram

--- histogram.py
+++ histogram.py
@@ -21,6 +21,45 @@ plt.rcParams.update({
 plt.style.use('shendrukGroupStyle')
 import shendrukGroupFormat as ed
 
+def plot_histogram(data, no_bins = 10, ylog = True, xlog = True, xlabel = 'Height, h (m)', ylabel = 'PDF(h)', output = 'histogram.png'):
+    '''
+    Display histogram of selected data set
+    
+    :param data: array
+            data of which histogram should be created from
+    :param no_bins: float
+            number of bins that data should be split into.
+    :param ylog: boolean
+            should the yscale be log?
+    :param xlog: boolean
+            should the xscale be log?
+    :param xlabel: string
+            x axis label
+    :param ylabel: string
+            y axis label
+    :param output: string
+            where the histrogram should be saved and output to
+    '''
+    
+    hist, bin_edges = np.histogram(h, bins = no_bins, density = True)
+    
+    bin_width = np.abs(bin_edges[0] - bin_edges[1])
+    
+    bin_centres = bin_edges[:-1] + 0.5*bin_width
+    
+    plt.scatter(bin_centres, hist)
+    
+    if ylog == True:
+        plt.yscale('log')
+        
+    if xlog == True:
+        plt.xscale('log')
+        
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    
+    plt.savefig(output, dpi = 300)
+
 # read in output file
 df = np.array(pd.read_csv('output.csv', sep = ',', header = None))
 #print(df)
@@ -36,18 +75,7 @@ R = float(constants[4]) # clinostat radius
 # reference point is -R 
 h = y + R # changes from x = 0 to x = R as reference point, i.e height is main measurement
 
-# counts and bin edges, normalised
-hist, bin_edges = np.histogram(h, bins = 10, density = True)
+# plot the histogram and save output
+plot_histogram(h, ylog = True, xlog = True, xlabel = 'Height, h (m)', ylabel = 'PDF(h)', output = 'histogram.png')
 
-# width of bin, all are equal in width
-bin_width = np.abs(bin_edges[0] - bin_edges[1])
-
-# bin centres
-bin_centres = bin_edges[:-1] + 0.5*bin_width
-
-plt.scatter(bin_centres, hist)
-plt.xscale('log')
-plt.yscale('log')
-plt.xlabel('Height, h (m)')
-plt.ylabel('PDF(h)')
 
